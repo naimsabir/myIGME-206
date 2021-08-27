8283ea2758b9b0db3681e9fdadcefe3ad4fa9640
naimsabir <gothamhush84@gmail.com>
2021-08-26 20:43:24 -0400

    Created and finsihed the Hello World Program

---
+++ Sabir_PE1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabir_PE1
+{
+    //Class: Program
+    //Author: Naim Sabir
+    //Purpose: To hold the main method which will display the "Hello World! message to the console
+    //Restrictions: None
+    static class Program
+    {
+        //Method: Main
+        //Purpose: Display the phrase "Hello World! to the users
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Hello World!");
+        }
+    }
+}
