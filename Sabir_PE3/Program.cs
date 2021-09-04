3629346d0697c4294dcdce08af2c22d0163b0e4d
naimsabir <gothamhush84@gmail.com>
2021-09-03 20:59:14 -0400

    Finished Homework Assignment 3

---
+++ Sabir_PE3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabir_PE3
+{
+    // Class Program
+    // Author: Naim Sabir
+    // Purpose: Homework Assignment 3
+    // Restrictions: None
+    class Program
+    {
+        // Method: Main
+        // Purpose: obtain four int values from the user
+        // and display the product
+        // Restrictions: None
+        static void Main(string[] args)
+        {
+            int val1, val2, val3, val4;
+            //string sVal1, sVal2, sVal3, sVal4;
+            Console.WriteLine("Enter your first integer.");
+            val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your second integer.");
+            val2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your third integer.");
+            val3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your fourth integer.");
+            val4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Integer 1 is {0}: \n Integer 2 is {1} \n Integer 3 is {2} \n Integer 4 is {3}", val1, val2, val3, val4);
+        }
+    }
+}
