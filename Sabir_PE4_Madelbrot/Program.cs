d934d2e87b11d3383725703c51ade95ff98d17b7
naimsabir <gothamhush84@gmail.com>
2021-09-06 17:40:04 -0400

    Finished Homework 4 Mandelbrot???

--- Sabir_PE4_Madelbrot/Program.cs
+++ Sabir_PE4_Madelbrot/Program.cs
@@ -27,12 +27,30 @@ namespace Mandelbrot
         [STAThread]
         static void Main(string[] args)
         {
+            //doubles that will hold the user generated start and end values for realCoord and imagCoord
+            //called userrealCoord and userimageCoord respectively
+            double userrealCoord, userimageCoord;
             double realCoord, imagCoord;
             double realTemp, imagTemp, realTemp2, arg;
             int iterations;
-            for (imagCoord = 1.2; imagCoord >= -1.2; imagCoord -= 0.05)
+            //prompt user for new start and end values
+            Console.WriteLine("Enter a value for realCoord:");
+            userrealCoord = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter a value for imageCoord:");
+            userimageCoord = Convert.ToDouble(Console.ReadLine());
+
+            //need to make sure that there are 48 values
+            //in order to maintain 48 values I use the user's input as the logic statement inside the for loop
+            //in order to be able to use the number they entered in a mathematic expression to ensure the same
+            //values regardless of the number the user input.
+            for (imagCoord = userimageCoord; imagCoord >= -(userimageCoord); imagCoord -= (userimageCoord/24))
             {
-                for (realCoord = -0.6; realCoord <= 1.77; realCoord += 0.03)
+                //need to make sure there are 80 values
+                //in order to maintain 48 values I use the user's input as the logic statement inside the for loop
+                //in order to be able to use the number they entered in a mathematic expression to ensure the same
+                //values regardless of the number the user input.
+                for (realCoord = -(userrealCoord); realCoord <= (userrealCoord); realCoord += (userrealCoord/40))
                 {
                     iterations = 0;
                     realTemp = realCoord;
