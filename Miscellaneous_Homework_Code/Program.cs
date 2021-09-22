230206b7b2026093b8020dca6848f1fb95615ac8
naimsabir <gothamhush84@gmail.com>
2021-09-21 21:37:47 -0400

    Finished Unit Test 1: Question 1

---
+++ Miscellaneous_Homework_Code/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscellaneous_Homework_Code
+{
+    //Class: Program
+    //Author: Naim Sabir
+    //Purpose: To be used as a project with the sole purpose of doing homework
+    //problems that involve code
+    //Restrictions: Has no real purpose
+    class Program
+    {
+        struct order
+        {
+          
+
+            public string itemName;
+            public int unitCount;
+            public double unitCost;
+
+            public double TotalPrice()
+            {
+                return unitCount * unitCost;
+            }
+     
+            public string Receipt()
+            {
+                string receipt = "{0} {1} items at ${2} each, total cost ${3}", unitCount, itemName, unitCost, TotalPrice();
+                return receipt;
+            }
+
+        }
+
+        //Method: Main
+        //Purpose: Same as above
+        //Restrictions: Same as above
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Sabir_UnitTest_1/Program.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabir_UnitTest_1
+{
+    //Class: Program
+    //Author: Naim Sabir
+    //Purpose: Unit Test 1: Question 1
+    //Restrictions: None
+    class Program
+    {
+        //Method: Main
+        //Purpose: Create a console application that enhances the attached "Math Quiz Application" to include division.
+        //Note that you will have to replace the int variables with doubles and you should round the quotient to 2
+        //decimal places and indicate that the user should enter their answer to 2 decimal places.  (dAnswer = Math.Round(dAnswer,2))
+        //Restrictions: Instead of being able to catch the user if they enter a number with more than two decimal places
+        //the program will only tell the user to use them if they don't want to get a wrong answer. Meaning that there is nothing
+        //to actually stop the user from entering over two decimal values
+
+        static void Main()
+        {
+            // store user name
+            string myName = "";
+
+            // string and int of # of questions
+            string sQuestions = "";
+            int nQuestions = 0;
+
+            // string and base value related to difficulty
+            string sDifficulty = "";
+            int nMaxRange = 0;
+
+            // constant for setting difficulty with 1 variable
+            const int MAX_BASE = 10;
+
+            // question and # correct counters
+            int nCntr = 0;
+            int nCorrect = 0;
+
+            // operator picker
+            int nOp = 0;
+
+            // operands and solution
+            //changed all the integer values to doubles
+            double val1 = 0;
+            double val2 = 0;
+            double nAnswer = 0;
+
+            // string and int for the response
+            //changed the int to a double because it will hold decimals now
+            string sResponse = "";
+            double nResponse = 0;
+
+            // boolean for checking valid input
+            bool bValid = false;
+
+            // play again?
+            string sAgain = "";
+
+            // seed the random number generator
+            Random rand = new Random();
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Math Quiz!");
+            Console.WriteLine();
+
+            // fetch the user's name into myName
+            while (true)
+            {
+                Console.Write("What is your name-> ");
+                myName = Console.ReadLine();
+
+                if (myName.Length > 0)
+                {
+                    break;
+                }
+            }
+
+        // label to return to if they want to play again
+        start:
+
+            // initialize correct responses for each time around
+            nCorrect = 0;
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("How many questions-> ");
+                sQuestions = Console.ReadLine();
+
+                try
+                {
+                    nQuestions = int.Parse(sQuestions);
+                    bValid = true;
+                }
+                catch
+                {
+                    Console.WriteLine("Please enter an integer.");
+                    bValid = false;
+                }
+
+            } while (!bValid);
+
+            Console.WriteLine();
+
+            do
+            {
+                Console.Write("Difficulty level (easy, medium, hard)-> ");
+                sDifficulty = Console.ReadLine();
+            } while (sDifficulty.ToLower() != "easy" &&
+                     sDifficulty.ToLower() != "medium" &&
+                     sDifficulty.ToLower() != "hard");
+
+            Console.WriteLine("And make sure to round to 2 decimal points if you want a correct answer.");
+            Console.WriteLine();
+
+            // if they choose easy, then set nMaxRange = MAX_BASE, unless myName == "David", then set difficulty to hard
+            // if they choose medium, set nMaxRange = MAX_BASE * 2
+            // if they choose hard, set nMaxRange = MAX_BASE * 3
+            switch (sDifficulty.ToLower())
+            {
+                case "easy":
+                    nMaxRange = MAX_BASE;
+                    if (myName.ToLower() == "david")
+                    {
+                        goto case "hard";
+                    }
+                    break;
+
+                case "medium":
+                    nMaxRange = MAX_BASE * 2;
+                    break;
+
+                case "hard":
+                    nMaxRange = MAX_BASE * 3;
+                    break;
+            }
+
+            // ask each question
+            for (nCntr = 0; nCntr < nQuestions; ++nCntr)
+            {
+                // generate a random number between 0 inclusive and 4 exclusive to get the operation
+                nOp = rand.Next(0, 4);
+
+                val1 = rand.Next(0, nMaxRange) + nMaxRange;
+                val2 = rand.Next(0, nMaxRange);
+
+                // if either argument is 0, pick new numbers
+                if (val1 == 0 || val2 == 0)
+                {
+                    // decrement counter to try this one again (because it will be incremented at the top of the loop)
+                    --nCntr;
+                    continue;
+                }
+
+                // if nOp == 0, then addition
+                // if nOp == 1, then subtraction
+                // else multiplication
+                if (nOp == 0)
+                {
+                    nAnswer = val1 + val2;
+                    sQuestions = $"Question #{nCntr + 1}: {val1} + {val2} => ";
+                }
+                else if (nOp == 1)
+                {
+                    nAnswer = val1 - val2;
+                    sQuestions = $"Question #{nCntr + 1}: {val1} - {val2} => ";
+                }
+                else if (nOp == 2)
+                {
+                    //if nOp == 2 divide value 1 and 2 and then round the answer to 2 decimal places
+                    nAnswer = val1 / val2;
+                    nAnswer = Math.Round(nAnswer, 2);
+                    sQuestions = $"Question #{nCntr + 1}: {val1} / {val2} => ";
+                }
+                else
+                {
+                    nAnswer = val1 * val2;
+                    sQuestions = $"Question #{nCntr + 1}: {val1} * {val2} => ";
+                }
+
+                // display the question and prompt for the answer
+                do
+                {
+                    Console.Write(sQuestions);
+                    sResponse = Console.ReadLine();
+
+                    try
+                    {
+                        nResponse = double.Parse(sResponse);
+                        bValid = true;
+                    }
+                    catch
+                    {
+                        Console.WriteLine("Please enter an integer.");
+                        bValid = false;
+                    }
+
+                } while (!bValid);
+
+                // if response == answer, output flashy reward and increment # correct
+                // else output stark answer
+                if (nResponse == nAnswer)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine("Well done, {0}!!!", myName);
+
+                    ++nCorrect;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("I'm sorry {0}. The answer is {1}", myName, nAnswer);
+                }
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            // output how many they got correct and their score
+            Console.WriteLine("You got {0} correct out of {1}, which is a score of {2:P2}", nCorrect, nQuestions, Convert.ToDouble(nCorrect) / (double)nCntr);
+            Console.WriteLine();
+
+            do
+            {
+                // prompt if they want to play again
+                Console.Write("Do you want to play again? ");
+
+                sAgain = Console.ReadLine();
+
+                if (sAgain.ToLower().StartsWith("y"))
+                {
+                    goto start;
+                }
+                else if (sAgain.ToLower().StartsWith("n"))
+                {
+                    break;
+                }
+            } while (true);
+        }
+    }
+}
