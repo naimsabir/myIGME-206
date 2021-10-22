56b7b37fe7055ad6863d037e80d766b145b0e799
naimsabir <gothamhush84@gmail.com>
2021-10-22 13:38:59 -0400

    Finished Unit Test 2 Question 14

---
+++ Crash_TestFile/Class1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crash_TestFile
+{
+    //Pseudo-Code
+    //Intro: You're making your first game. YESS!!!!!!!!!!!!
+    //I have no idea what i'm doing but this is to lay out the steps of what I need done (and adjust as I realize I need more
+    //1. Need a player movement system that makes the player move forward continuously and allowing them to jump
+    //and maneuver in the air.
+    //2. Need obstacles that come at the player from the direction they are running
+    //3. Need powerups to influence the game
+    //4. Need multiple gameObjects for sound effects etc
+    //5. A level system and a scoring system
+    //6. A start screen and an end screen allowing you to restart the game. No checkpoint system because I have no idea how the fuck to do that.
+    //7. An infinitely repeating stage. Gonna definetly need to do some research for this one because I have no idea how I would even do that.
+    //8. The camera would need to follow the player at all points in time so I'd need to create a focal point.
+    //9. Generally things that involve the movement of things in the game space should not be in the dll file.
+    //10. Can handle procedural map generation here
+
+    //A class dedicated to keeping score, and incrementing the score using a method call ScoreUp 
+    //that can be called in other scripts. Can potentially also make a decrementer if I get far
+    //enough in my design
+    public class EnemyGenerator
+    {
+        object enemy1;
+        object enemy2;
+        object enemy3;
+        public int randomNum;
+        Random rand = new Random();
+        public EnemyGenerator()
+        {
+            randomNum = rand.Next(0, 2);
+
+            if(randomNum == 0)
+            {
+                SpawnEnemy(enemy1);
+            }
+            if (randomNum == 1)
+            {
+                SpawnEnemy(enemy2);
+            }
+            if (randomNum == 2)
+            {
+                SpawnEnemy(enemy3);
+            }
+        }
+
+        public void SpawnEnemy(object enemy)
+        {
+            //I don't know how I would represent the unity logic to spawn an enemy so this will be empty
+        }
+
+    }
+    //public class PlayerMovement
+    //{
+    //    //speed that increments based on whether the player is holding down the right arrow key/ d key
+    //    public int speed;
+    //    //if no key is being pressed have the player move slowly to the right
+    //    //if the right arrow or the d key is pressed have the player move faster to the right.
+    //}
+    public class ScoreController
+    {
+        //start the score at 0
+        public int score = 0;
+
+        //in case I need to manually set the score at some point in time
+        public void SetScore(int newScore)
+        {
+            score = newScore;
+        }
+        public int GetScore()
+        {
+            return score;
+        }
+        public void ScoreUp()
+        {
+            //can potentially add logic to increase the score by different amounts if it's called for
+            //score increases by 100
+            score += 100;
+        }
+
+    }
+    public class LevelController : ScoreController
+    {
+        ScoreController scoreController = new ScoreController();
+        //start at level 1
+        public int currentLevel = 1;
+
+        public void NextLevel()
+        {
+            //I know this method can go without the if statement but I'm keeping it here so I know what to do later on
+            if(scoreController.GetScore() == 1000)
+            {
+                currentLevel++;
+            }
+        }
+
+    }
+    public class Class1
+    {
+    }
+}
---
+++ Sabir_UnitTest2_1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabir_UnitTest2_1
+{
+    //struct Friend
+    //{
+    //    public string name;
+    //    public string greeting;
+    //    public DateTime birthdate;
+    //    public string address;
+    //}
+
+    //Class: Friend
+    //Author: Naim Sabir
+    //Purpose: Unit Test 2 Question 14
+    //Restrictions: None
+    public class Friend : ICloneable
+    {
+        //keeping the same fields as the struct
+        public string name;
+        public string greeting;
+        public DateTime birthdate;
+        public string address;
+
+        //Creating a clone method to perform a deep copy
+        public Object Clone()
+        {
+            //create a friend object to copy stuff into
+            //don't need any other things added to copy because everything is inside of the friend class
+            Friend copy = (Friend)this.MemberwiseClone();
+
+            return copy;
+        }
+    
+    }
+    
+
+    //Class: Program
+    //Author: Naim Sabir
+    //Purpose: Unit Test 2
+    //Restrictions: None
+    class Program
+    {
+     
+        //Method: Main
+        //Purpose: Unit Test 2 #14
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            //Write a console application which converts the following code from using
+            //struct Friend to public class Friend and generates the same output:
+
+            //could potentially just make methods for adding values to the Friend class fields
+            //methods would be Name, Greeting, Birthdate, and Address
+
+            //change from struct objects to class objects
+
+            Friend friend = new Friend();
+            Friend enemy = new Friend();
+
+            //Friend friend;
+            //Friend enemy;
+
+            // create my friend Charlie Sheen
+            friend.name = "Charlie Sheen";
+            friend.greeting = "Dear Charlie";
+            friend.birthdate = DateTime.Parse("1967-12-25");
+            friend.address = "123 Any Street, NY NY 12202";
+
+            //calling the clone method in the Friend class to deep copy
+            // now he has become my enemy
+            enemy = (Friend)friend.Clone();
+
+            // set the enemy greeting and address without changing the friend variable
+            enemy.greeting = "Sorry Charlie";
+            enemy.address = "Return to sender.  Address unknown.";
+
+            Console.WriteLine($"friend.greeting => enemy.greeting: {friend.greeting} => {enemy.greeting}");
+            Console.WriteLine($"friend.address => enemy.address: {friend.address} => {enemy.address}");
+
+            //The original output: friend.greeting => enemy.greeting: Dear Charlie => Sorry Charlie
+            //friend.address => enemy.address: 123 Any Street, NY NY 12202 => Return to sender.Address unknown.
+
+            //The previous output: friend.greeting => enemy.greeting: Sorry Charlie => Sorry Charlie
+            //friend.address => enemy.address: Return to sender.Address unknown. => Return to sender.Address unknown.
+
+            //need to deep copy
+
+            //The current output:friend.greeting => enemy.greeting: Dear Charlie => Sorry Charlie
+            //friend.address => enemy.address: 123 Any Street, NY NY 12202 => Return to sender.Address unknown.
+
+        }
+
+    }
+}
