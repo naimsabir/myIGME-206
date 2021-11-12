9261bbb30e580a957f883d7f5ceb8051ba16fb78
naimsabir <gothamhush84@gmail.com>
2021-11-12 15:39:26 -0500

    Got somewhere with PE-22

---
+++ Sabir_PE22/Program.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Timers;
+
+namespace Sabir_PE22
+{
+    class Trivia
+    {
+        public int response_code;
+        public List<TriviaResult> results;
+    }
+
+    class TriviaResult
+    {
+        public string category;
+        public string type;
+        public string difficulty;
+        public string question;
+        public string correct_answer;
+        public List<string> incorrect_answers;
+    }
+
+    class Program
+    {
+        static Timer timer;
+        static bool isTimeUp;
+        // neighboring points can be determined by if (maxtrixGraph[x, y].Item1 == x)
+        // relative direction is Item1 in the tuple
+        // cost is Item2 in the tuple
+        static (string, int)[,] matrixGraph = new (string, int)[,]
+        {
+                 //    A           B           C           D           E           F           G           H
+          /*A*/  {("NE", 0),  ("S", 2),   (null, -1), (null, -1), (null, -1), (null, -1), (null, -1), (null, -1)},
+          /*B*/  {(null, -1), (null, -1), ("S", 2),   ("E", 3),   (null, -1), (null, -1), (null, -1), (null, -1)},
+          /*C*/  {(null, -1), ("N", 2),   (null, -1), (null, -1), (null, -1), (null, -1), (null, -1), ("S", 20)},
+          /*D*/  {(null, -1), ("W", 3),   ("S", 5),   (null, -1), ("N", 2),   ("E", 4),   (null, -1), (null, -1)},
+          /*E*/  {(null, -1), (null, -1), (null, -1), (null, -1), (null, -1), ("S", 3),   (null, -1), (null, -1)},
+          /*F*/  {(null, -1), (null, -1), (null, -1), (null, -1), (null, -1), (null, -1), ("E", 1),   (null, -1)},
+          /*G*/  {(null, -1), (null, -1), (null, -1), (null, -1), ("N", 0),   (null, -1), (null, -1), ("S", 2)},
+          /*H*/  {(null, -1), (null, -1), (null, -1), (null, -1), (null, -1), (null, -1), (null, -1), (null, -1)}
+        };
+
+        //Item1 is the index of the neighbor, Item2 is the direction and Item3 is the cost
+        static (int, string, int)[][] listGraph = new (int, string, int)[][]
+        {
+            /*A*/ new (int, string, int)[] {(0, "N", 0), (0, "E", 0), (1, "S", 2)},
+            /*B*/ new (int, string, int)[] {(2, "S", 2), (3, "E", 3)},
+            new (int, string, int)[] {(1, "N", 2), (7, "S", 20)},
+            new (int, string, int)[] {(1, "W", 3), (2, "S", 5), (4, "N", 2), (5, "E", 4)},
+            new (int, string, int)[] {(5, "S", 3)},
+            new (int, string, int)[] {(6, "E", 1)},
+            new (int, string, int)[] {(4, "N", 0)},
+            null
+        };
+
+        // parallel arrays to store the weight, direction and room indexes
+        // weight graph
+        static int[][] wGraph = new int[][]
+        {
+            /*A*/ new int[] {0, 0, 2}
+            /*B new (int, string, int)[] {(2, "S", 2), (3, "E", 3)},
+            new (int, string, int)[] {(1, "N", 2), (7, "S", 20)},
+            new (int, string, int)[] {(1, "W", 3), (2, "S", 5), (4, "N", 2), (5, "E", 4)},
+            new (int, string, int)[] {(5, "S", 3)},
+            new (int, string, int)[] {(6, "E", 1)},
+            new (int, string, int)[] {(4, "N", 0)},
+            null  */
+        };
+
+        // room graphs: contains the indexes of the connected rooms
+        static int[][] iGraph = new int[][]
+        {
+            /*A*/ new int[] {0, 0, 1}
+            /*B new (int, string, int)[] {(2, "S", 2), (3, "E", 3)},
+            new (int, string, int)[] {(1, "N", 2), (7, "S", 20)},
+            new (int, string, int)[] {(1, "W", 3), (2, "S", 5), (4, "N", 2), (5, "E", 4)},
+            new (int, string, int)[] {(5, "S", 3)},
+            new (int, string, int)[] {(6, "E", 1)},
+            new (int, string, int)[] {(4, "N", 0)},
+            null  */
+        };
+
+        //can create a static string[,] = new string[,] to store descriptions
+        static string[] desc = new string[]
+        {
+            /* A */ "You wake up in a dark damp cave, with no idea how you arrived there. \n To the south of you " +
+                "there is an opening and every other direction is closed off. Which direction will you head? North, South, or East.",
+
+            /* B */ "You exit the cave opening into what seems to be an abandoned underground laboratory. \n" +
+                " After looking around you see that there are doors east and south of you. Which direction will you head? ",
+
+            /* C */ "You enter a room that looks like it's used to dispose of failed experiments. There is what looks like a defunct long incinerator tunnel" +
+                " to the south that seems to lead somewhere.\n To the north is the door you came through. Which direction will you head?",
+
+            /* D */ "You enter a room filled with medical equipment that has a bloody table in the middle of it.\n There is a door to the east, the door you" +
+                " came through to the west, a door to the north, and what looks like a strange human sized chute at the foot of the table.\n Which direction will you head?",
+
+            /* E */ "You enter a large room filled with dozens of open prison cells along the walls, some with skeletons and blood stains. \n " +
+                "The door you came through was locked behind you and so is the door ahead of you. However, the door to the south has automatically opened." +
+                "\n Beyond it is a hallway filled with lasers to pass through. Which direction will you head?",
+
+            /* F */ "You enter a room narrowly abvoiding the large vat of acid in the middle of the room. \n the doors around you all lock except for one" +
+                " to the east that opens. Whic direction will you head?",
+
+            /* G */ " You enter a room fiiled with bodies. Some look like prisoners and some look like guards all of which seemed to be crawling towards a door to the South" +
+                "\n the hall it leads to seems to be filled with gas but has a little light at the end. To the north there is another less ominous door. \n " +
+                "Which direction will you head?",
+
+            /* H */ "You powered through the gas and escaped",
+        };
+
+        static string RandomPhrase(int picker)
+        {
+            string randPhrase;
+            switch(picker)
+            {
+                case 1:
+                    randPhrase = "As you walk to the door you are sprayed with acid!";
+                    return randPhrase;
+                case 2:
+                    randPhrase = "A shambling mutated experiment claws at you before dying!";
+                    return randPhrase;
+                case 3:
+                    randPhrase = "You stepped on a nail!";
+                    return randPhrase;
+                case 4:
+                    randPhrase = "You triggered a booby trap!";
+                    return randPhrase;
+                case 5:
+                    randPhrase = "You inhaled toxic gas!";
+                    return randPhrase;
+                default:
+                    randPhrase = "Naim somehow fucked up the most simple part of this!";
+                    return randPhrase;
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+
+            timer = new Timer(15000);
+
+            
+            
+            Random rand = new Random();
+
+            ElapsedEventHandler elapsedEventHandler = new ElapsedEventHandler(TimesUp);
+
+            int randDamage;
+
+            string randPhrase;
+
+            int nRoom = 0;
+
+            int playerHp = 1;
+
+            //possibly add and playerHp != 0 to this
+            while (nRoom != 7 && playerHp > 0)
+            {
+                // if not room A and not room H then randomly reduce their HP such that they don't die
+                if(nRoom != 0)
+                {
+                    randDamage = rand.Next(0, 5);
+                    randPhrase = RandomPhrase(rand.Next(1, 5));
+
+                    playerHp = playerHp - randDamage;
+
+                    Console.WriteLine($"{randPhrase} You took {randDamage} damage!");
+                }
+
+                // display a desc of the room
+                 Console.WriteLine(desc[nRoom]);
+
+                // display any exits from the room
+                (int, string, int)[] thisRoomsNeighbors = listGraph[nRoom];
+
+                int nExits = 0;
+
+                foreach ((int, string, int) neighbor in thisRoomsNeighbors)
+                {
+                    if (playerHp > neighbor.Item3)
+                    {
+                        Console.WriteLine("There is an exit to the " + neighbor.Item2 + " which costs " + neighbor.Item3 + "HP");
+                        ++nExits;
+                    }
+                }
+
+                // display the hp
+                Console.WriteLine($"You have {playerHp} HP");
+
+                // ask the player if they want wager (w) for more hp or leave (l) the room only if there are nExits > 0
+                Console.WriteLine("Do you want to wager your health with trivia? Or do you want to exit the room?\n Enter w to wager and l to leave the room");
+                string sResponse;
+
+                sResponse = Console.ReadLine();
+
+                if (sResponse.ToLower() == "l" /* leaving room */ && nExits > 0 /* if there are exits */ )
+                {
+                    bool bValid = false;
+                    string sDirection;
+
+                    Console.WriteLine("Enter your chosen direction");
+
+                    while (!bValid)
+                    {
+                        sDirection = Console.ReadLine();
+
+                        for (int nCntr = 0; nCntr < 8; ++nCntr)
+                        {
+                            if (matrixGraph[nRoom, nCntr].Item1.Contains(sDirection) && playerHp > matrixGraph[nRoom, nCntr].Item2)
+                            {
+                                nRoom = nCntr;
+                                playerHp -= matrixGraph[nRoom, nCntr].Item2;
+                                bValid = true;
+                                break;
+                            }
+                        }
+
+                        if (!bValid)
+                        {
+                            Console.WriteLine("That isn't a valid direction");
+                        }
+                    }
+                }
+                else
+                {
+                    // trivia question
+                    // fetch api
+                    // 15 second limit to answer
+                    // multiple choice 1-4
+
+                    // ask player how much HP to wager (limited to playerHp)
+                    // prevent player from wagering a negative number
+                    int healthWager;
+
+                    wager:
+
+                    Console.WriteLine("How much health do you want to wager?");
+                    healthWager = Convert.ToInt32(Console.ReadLine());
+
+                    if(healthWager <= 0)
+                    {
+                        Console.WriteLine("Cannot enter a negative number. Try again!");
+                        goto wager;
+                    }
+
+                    int playerAnswer, rightNum = 0;
+                    string url = null;
+                    string s = null;
+
+                    HttpWebRequest request;
+                    HttpWebResponse response;
+                    StreamReader reader;
+
+                    url = "https://opentdb.com/api.php?amount=1&type=multiple";
+
+                    request = (HttpWebRequest)WebRequest.Create(url);
+                    response = (HttpWebResponse)request.GetResponse();
+                    reader = new StreamReader(response.GetResponseStream());
+                    s = reader.ReadToEnd();
+                    reader.Close();
+
+                    Trivia trivia = JsonConvert.DeserializeObject<Trivia>(s);
+
+                    trivia.results[0].question = HttpUtility.HtmlDecode(trivia.results[0].question);
+                    trivia.results[0].correct_answer = HttpUtility.HtmlDecode(trivia.results[0].correct_answer);
+                    for (int i = 0; i < trivia.results[0].incorrect_answers.Count; ++i)
+                    {
+                        trivia.results[0].incorrect_answers[i] = HttpUtility.HtmlDecode(trivia.results[0].incorrect_answers[i]);
+                    }
+
+                    // put the answers in random order
+                    // prefix each answer with number 1-4 so the player only need to type the number
+                    Console.WriteLine($"The question is {trivia.results[0].question}");
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        int rightOrWrong;
+                        bool rightOnBoard = false;
+                        rightOrWrong = rand.Next(1,2);
+                        if (rightOrWrong == 1 || rightOnBoard == true)
+                        {
+                            Console.WriteLine($"Number{i}: {trivia.results[0].incorrect_answers[rand.Next(0, trivia.results[0].incorrect_answers.Count)]}");
+                        }
+                        if(rightOrWrong == 2 && rightOnBoard == false)
+                        {
+                            Console.WriteLine($"Number{i}: {trivia.results[0].correct_answer}");
+                            //so only one correct answer can show up
+                            rightOnBoard = true;
+                            rightNum = i;
+                        }
+                    }
+                    timer.Elapsed += elapsedEventHandler;
+                    timer.Start();
+
+                    Console.WriteLine("Enter the number of the correct answer");
+                    playerAnswer = Convert.ToInt32(Console.ReadLine());
+
+                    if(playerAnswer == rightNum && isTimeUp == false)
+                    {
+                        Console.WriteLine("Correct!");
+                        playerHp += healthWager;
+                    }
+                    else if(playerAnswer != rightNum || isTimeUp == true)
+                    {
+                        Console.WriteLine($"That answer was incorrect the correct answer was number: {rightNum}");
+                        playerHp -= healthWager;
+                    }
+                    
+                    // 15 second timer
+
+
+                }
+            }
+            if(nRoom == 7)
+            {
+                Console.WriteLine("You have escaped!");
+            }
+            if(playerHp <= 0)
+            {
+                Console.WriteLine("You have died");
+            }
+        }
+         static void TimesUp(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Times Up!");
+            isTimeUp = true;
+            timer.Stop();
+        }
+
+    }
+}
---
+++ Sabir_PE22_2/Program.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Web;
+
+namespace Sabir_PE22_2
+{
+    class Program
+    {
+        //Write the C# code to represent this digraph as both an Adjacency Matrix and as an Adjacency List.
+        //Note that you will have to represent the neighboring points, their relative direction, and their cost.
+
+        //matrix list
+        static int[,] mGraph = new int[,]
+        {
+                    /* A */ /* B */ /* C */ /* D */ /* E */ /* F */ /* G */ /* H */
+            /* A */ {  0,      2,     -1,     -1,     -1,     -1,     -1,     -1, },
+            /* B */ { -1,     -1,      2,      3,     -1,     -1,     -1,     -1, },
+            /* C */ { -1,      2,     -1,     -1,     -1,     -1,     -1,     20, },
+            /* D */ { -1,     -1,      5,     -1,      2,      4,     -1,     -1, },
+            /* E */ { -1,     -1,     -1,     -1,     -1,      3,     -1,     -1, },
+            /* F */ { -1,     -1,     -1,     -1,     -1,     -1,      1,     -1, },
+            /* G */ { -1,     -1,     -1,     -1,      0,     -1,     -1,      2, },
+            /* H */ { -1,     -1,     -1,     -1,     -1,     -1,     -1,     -1, },
+        };
+
+        //adjacency list
+        static int[][] lGraph = new int[][]
+        {
+            /* A */ new int[] { 0, 1 },    /* A, B */
+            /* B */ new int[] { 2, 3 },    /* C, D */
+            /* C */ new int[] { 1, 7 }, /* B, H */
+            /* D */ new int[] { 2, 4, 5 },    /* C, E, F */
+            /* E */ new int[] { 5}, /* F */
+            /* F */ new int[] { 6}, /* G */
+            /* G */ new int[] { 4, 7}, /* E, H */
+            /* H */ new int[] {},
+        };
+
+        //edge weights
+        static int[][] wGraph = new int[][]
+        {
+            /* A */ new int[] {0, 2},
+            /* B */ new int[] {2, 3},
+            /* C */ new int[] {2, 20},
+            /* D */ new int[] {5, 2, 4},
+            /* E */ new int[] {3},
+            /* F */ new int[] {1},
+            /* G */ new int[] {0, 2},
+            /* H */ new int[] {},
+        };
+
+        static void Main(string[] args)
+        {
+            /*1. Start the player in Room A with an initial Health Point value of 1.
+              2. Upon entering a room that is not A or H, something should happen that decreases their HP a random amount,
+                 but not such that their HP will reach less than 1.  
+                       Examples of injuries are an attack from a bat, the player trips on a rock, etc.
+              3. The game will provide a unique and interesting description of each room without including the room letter (not "You are in room B"…),
+                 and the description will include the exits to which the player has access based on their (HP - 1).  The player will not see the map, 
+                 so they are discovering the game based on your descriptions of each room.  The description should also display the player's current HP.
+              4. Prompt the player for what they want to do: leave via an available exit or wager their HP against a trivia question.
+              5. If they choose an exit, then deduct the cost of that exit from their HP and move them to the next room.  Goto step 2.
+              6. If they choose to grind for HP, then prompt the player how much of their HP they want to wager, and use the trivia JSON API at
+                 https://opentdb.com/api_config.php to generate a multiple choice question to ask the player.  (Refer to TriviaApp in Session 11-1).  
+                 If they get the question right, then their HP increases by their wager.  If they get it wrong then their HP reduces by their wager, 
+                 and they die if their HP reaches 0.  They should have only 15 seconds to answer the question.  
+                 (You can set the trivia parameters or just use https://opentdb.com/api.php?amount=1 - you only need 1 question).
+              7. Goto step 3 */
+
+            Random rand = new Random();
+
+            int nState, playerHP = 1;
+            string direction;
+
+
+            Console.WriteLine("You wake up in a dark damp cave, with no idea how you arrived there. \n To the south of you " +
+                "there is an opening and every other direction is closed off. Which direction will you head? North South East or West.");
+
+        roomA:
+
+            direction = Console.ReadLine().ToLower();
+
+
+
+            if (direction.StartsWith("e"))
+            {
+                nState = 0;
+                if (mGraph[0, nState] != -1)
+                {
+                    playerHP = playerHP - (mGraph[0, nState]);
+                    Console.WriteLine($"You wander to the east of the cave, there is a poorly drawn image of a person heading into the opening \n" +
+                        $"Your health is {playerHP} and you have taken {mGraph[0, nState]} damage. Which direction will you head? North South East or West.");
+                    goto roomA;
+                }
+            }
+            if (direction.StartsWith("w"))
+            {
+                nState = 0;
+                if (mGraph[0, nState] != -1)
+                {
+                    playerHP = playerHP - (mGraph[0, nState]);
+                    Console.WriteLine($"You wander to the west of the cave, the cave wall is completely barren \n" +
+                        $"Your health is {playerHP} and you have taken {mGraph[0, nState]} damage. Which direction will you head? North South East or West.");
+                    goto roomA;
+                }
+            }
+            if (direction.StartsWith("s"))
+            {
+                nState = 1;
+                if (mGraph[0, nState] != -1)
+                {
+                    Console.WriteLine($"You go through the opening to the south. Your health is {playerHP} and you have taken {mGraph[0, nState]} damage.");
+                }
+            }
+            if (direction.StartsWith("n"))
+            {
+                nState = 0;
+                if (mGraph[0, nState] != -1)
+                {
+                    playerHP = playerHP - (mGraph[0, nState]);
+                    Console.WriteLine($"You wander to the north of the cave, the cave wall is completely barren \n" +
+                        $"Your health is {playerHP} and you have taken {mGraph[0, nState]} damage. Which direction will you head? North South East or West.");
+                    goto roomA;
+                }
+            }
+            //**********************************************************************ROOM B*************************************************************************
+
+            Console.WriteLine("You exit the cave opening into what seems to be an abandoned underground laboratory." +
+                " After looking around you see that there are doors east and south of you. Which direction will you head? ");
+
+            roomB:
+
+            direction = Console.ReadLine().ToLower();
+
+            if (direction.StartsWith("e"))
+            {
+                nState = 3;
+                if (mGraph[1, nState] != -1)
+                {
+                    playerHP = playerHP - (mGraph[1, nState]);
+                    Console.WriteLine($"You enter the door to the east. \n" +
+                        $"Your health is {playerHP} and you have taken {mGraph[0, nState]} damage.");
+                    goto roomB;
+                }
+                else
+                {
+                    Console.WriteLine("There is a dead end. Choose a new direction");
+                    goto roomB;
+                }
+            }
+            if (direction.StartsWith("w"))
+            {
+                nState = 0;
+                if (mGraph[1, nState] != -1)
+                {
+                    playerHP = playerHP - (mGraph[0, nState]);
+                    Console.WriteLine($"You wander to the west of the cave, the cave wall is completely barren \n" +
+                        $"Your health is {playerHP} and you have taken {mGraph[0, nState]} damage. Which direction will you head? North South East or West.");
+                    goto roomB;
+                }
+            }
+            if (direction.StartsWith("s"))
+            {
+                nState = 1;
+                if (mGraph[1, nState] != -1)
+                {
+                    Console.WriteLine($"You go through the opening to the south. Your health is {playerHP} and you have taken {mGraph[0, nState]} damage.");
+                }
+            }
+            if (direction.StartsWith("n"))
+            {
+                nState = 0;
+                if (mGraph[1, nState] != -1)
+                {
+                    playerHP = playerHP - (mGraph[0, nState]);
+                    Console.WriteLine($"You wander to the north of the cave, the cave wall is completely barren \n" +
+                        $"Your health is {playerHP} and you have taken {mGraph[0, nState]} damage. Which direction will you head? North South East or West.");
+                    goto roomB;
+                }
+            }
+        }
+    }
+}
